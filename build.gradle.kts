4682f82a9a1f7457937bc3251065d2603f1f7431
Gurkirst Singh <tbhaxor@gmail.com>
2024-11-16 00:17:42 +0530

    Make gradlew executable

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 tasks {
     shadowJar {
         archiveBaseName.set("SpoofProof")
-        archiveVersion.set("1.0.1")
+        archiveVersion.set("1.0.2")
         archiveClassifier.set("")
     }
 }
